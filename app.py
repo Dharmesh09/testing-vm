804454859d53cc25749343a6682702d53ffcf1da
Dharmesh09 <96689873+Dharmesh09@users.noreply.github.com>
2024-08-27 21:13:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, World!"
+    return "Hello, World! This is to test web app on the Virtual Machine"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80)
