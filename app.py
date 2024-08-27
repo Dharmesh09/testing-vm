415e1871ded5c221856090ce7e63e1d9cfa2b9aa
Dharmesh09 <96689873+Dharmesh09@users.noreply.github.com>
2024-08-27 21:30:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def hello():
     return "Hello, World! This is to test web app on the Virtual Machine"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(host='0.0.0.0', port=5000)
